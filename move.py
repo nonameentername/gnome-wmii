7cca3eb9df52f0530cc7f9578c5b1655da61ba0a
werner mendizabal <nonameentername@gmail.com>
2018-05-01 00:36:05 -0500

    Update to use latest gnome3 libraries

--- move.py
+++ move.py
@@ -1,39 +1,56 @@
 #!/usr/bin/env python
 
-import gtk
+import gi
 import sys
-import wnck
+gi.require_version('Gdk', '3.0')
+gi.require_version('Gtk', '3.0')
+gi.require_version('Wnck', '3.0')
+from gi.repository import Gdk
+from gi.repository import GdkX11
+from gi.repository import Gtk
+from gi.repository import Wnck
 
 # Get screen - this must come before gtk loop
-screen = wnck.screen_get_default()
-gtk_screen = gtk.gdk.screen_get_default()
+screen = Wnck.Screen.get_default()
+gtk_screen = Gdk.Screen.get_default()
 
 # Deal with pending events
-while gtk.events_pending():
-    gtk.main_iteration()
+while Gtk.events_pending():
+    Gtk.main_iteration()
 
 # Get windows list and filter for normal windows
 windows = screen.get_windows_stacked()
 filtered_windows = [
     window for window in windows
-    if window.get_window_type() == wnck.WindowType.__enum_values__[0]]
+    if window.get_window_type() == Wnck.WindowType.__enum_values__[0]]
 filtered_windows.reverse()
 
-active_window = wnck.screen_get_default().get_active_window()
-gdk_window = gtk.gdk.window_foreign_new(active_window.get_xid())
-monitor = gtk_screen.get_monitor_at_window(gdk_window)
+active_window = Wnck.Screen.get_default().get_active_window()
+
+if not active_window:
+    exit()
+
+gdk_display = GdkX11.X11Display.get_default()
+gdk_window = GdkX11.X11Window.foreign_new_for_display(
+    gdk_display, active_window.get_xid())
+gdk_monitor = gdk_display.get_monitor_at_window(gdk_window)
 
 monitor_windows = {}
 
-number_monitors = gtk.gdk.screen_get_default().get_n_monitors()
+number_monitors = gdk_display.get_n_monitors()
+
+for number in range(number_monitors):
+    if gdk_monitor == gdk_display.get_monitor(number):
+        monitor = number
 
 for number in range(number_monitors):
     monitor_windows[number] = []
 
 gdk_windows = gtk_screen.get_toplevel_windows()
 for window in filtered_windows:
-    if not isinstance(window, gtk.gdk.Window):
-        gdk_window = gtk.gdk.window_foreign_new(window.get_xid())
+    if not isinstance(window, Gdk.Window):
+        gdk_window = GdkX11.X11Window.foreign_new_for_display(
+            gdk_display, window.get_xid())
 
     current_monitor = gtk_screen.get_monitor_at_window(gdk_window)
     monitor_windows[current_monitor].append(window)
@@ -41,7 +58,7 @@ for window in filtered_windows:
 
 direction = sys.argv[1]
 
-now = gtk.gdk.x11_get_server_time(gtk.gdk.get_default_root_window())
+now = GdkX11.x11_get_server_time(Gdk.get_default_root_window())
 
 if direction == 'left':
     new_monitor = monitor - 1
@@ -50,7 +67,7 @@ if direction == 'left':
         windows = monitor_windows[new_monitor]
 
         if len(windows) > 0:
-            wnck.window_get(windows[0].get_xid()).activate(now)
+            Wnck.Window.get(windows[0].get_xid()).activate(now)
 
 if direction == 'right':
     new_monitor = monitor + 1
@@ -59,7 +76,7 @@ if direction == 'right':
         windows = monitor_windows[new_monitor]
 
         if len(windows) > 0:
-            wnck.window_get(windows[0].get_xid()).activate(now)
+            Wnck.Window.get(windows[0].get_xid()).activate(now)
 
 if direction == 'up':
     windows = sorted(monitor_windows[monitor])
@@ -73,7 +90,7 @@ if direction == 'up':
     if window_index < 0:
         window_index = len(windows) - 1
 
-    wnck.window_get(windows[window_index].get_xid()).activate(now)
+    Wnck.Window.get(windows[window_index].get_xid()).activate(now)
 
 if direction == 'down':
     windows = sorted(monitor_windows[monitor])
@@ -87,4 +104,4 @@ if direction == 'down':
     if window_index >= len(windows):
         window_index = 0
 
-    wnck.window_get(windows[window_index].get_xid()).activate(now)
+    Wnck.Window.get(windows[window_index].get_xid()).activate(now)
--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-left "['<Shift><Super>h']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-right "['<Shift><Super>l']"
 gsettings set org.gnome.desktop.wm.keybindings maximize "['<Shift><Super>k', '<Super>Up', '<Primary><Alt>KP_5']"
+gsettings set org.gnome.desktop.wm.keybindings minimize "[]"
+gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver "[]"
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/']"
 
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Move focus left'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "'<Super>h'"
